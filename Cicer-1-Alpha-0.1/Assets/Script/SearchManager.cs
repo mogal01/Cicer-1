bd22b9d8d3675d2985dad004deba9734fde1798f
pedrosed676 <145976423+pedrosed676@users.noreply.github.com>
2024-01-16 16:56:50 +0100

    Aggiunta di Checkpoint e implementazione struttura errore menù eventi

--- Cicer-1-Alpha-0.1/Assets/Script/SearchManager.cs
+++ Cicer-1-Alpha-0.1/Assets/Script/SearchManager.cs
@@ -10,6 +10,9 @@ public class SearchManager : MonoBehaviour
     public GameObject[] Element;
     public GameObject SearchBar;
     public int totalElements;
+    private bool isEmpty;
+    public GameObject erroreRicerca;
+
     // Start is called before the first frame update
 
     public void popola()
@@ -25,9 +28,12 @@ public class SearchManager : MonoBehaviour
     
     public void Search()
     {
+        isEmpty=true;
+        erroreRicerca.SetActive(false);
         string SearchText = SearchBar.GetComponent<TMP_InputField>().text;
         int searchTxtLength = SearchText.Length;
         int searchedElements = 0;
+    
         foreach(GameObject ele in Element)
         {
             searchedElements++;
@@ -36,14 +42,20 @@ public class SearchManager : MonoBehaviour
                 if(ele.GetComponentInChildren<Text>().text.ToLower().Contains(SearchText.ToLower()))
                 {
                     ele.SetActive(true);
+                    isEmpty=false;
                 }
                 else
                 {
                     ele.SetActive(false);
                 }
-            }
 
+            }
         }
+        if(isEmpty){
+
+                erroreRicerca.SetActive(true);
+        }
+        
     }
     // Update is called once per frame
     
